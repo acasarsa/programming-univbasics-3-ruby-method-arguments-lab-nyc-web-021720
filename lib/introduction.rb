2d2d5285764b47ca73420cbfa09a3311495c5282
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-30 16:22:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language (name, language)
 end
 
 def introduction_with_language_optional (name, language = "Ruby")
-  puts introduction_with_language
+  puts #{introduction_with_language}
 end
 
 
