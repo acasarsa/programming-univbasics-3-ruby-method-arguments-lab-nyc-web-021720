54cad26ee0c1eae50077eefda8691401c7eec170
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-30 16:26:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction (name)
   puts "Hi, my my name is #{name}."
-ends
+end
 
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
